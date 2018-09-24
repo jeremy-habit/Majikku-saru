cef6d5e51a71a2fa6b22d2215a1566c140fb0aeb
Jeremy Habit <jeremy.habit@gmail.com>
2018-09-24 13:03:10 +0200

    monkey zero

---
+++ index.js
@@ -0,0 +1,27 @@
+/* ::::: import with class */
+
+/* example :
+
+---
+import MagicMonkey from 'magic-monkey'
+new MagicMonkey().load()
+---
+
+*/
+
+/*
+class MagicMonkey {
+
+  constructor () {
+    /!*this.load()*!/
+  }
+
+  load () {
+    require("./stylesheets/magic-monkey.min.css")
+  }
+}
+
+module.exports = MagicMonkey*/
+
+
+require('./stylesheets/magic-monkey.min.css')
